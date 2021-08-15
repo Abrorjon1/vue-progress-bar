07f436215434b33056e49b8985e6f565e37030ad
Mirjalol Salimov official page <79058363+offism@users.noreply.github.com>
2021-08-15 23:14:26 +0300

    1c

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,63 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div id="app">
+    <main>
+      <div class="navigation">
+        <header>
+          <h1>Samar Badriddinov</h1>
+        </header>
+        <nav>
+          <a href="#">About Us</a>
+          <a href="#">Contact</a>
+          <a href="#">Portfolio</a>
+        </nav>
+      </div>
+      <Post />
+    </main>
+  </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+import Post from './components/Post'
 export default {
-  name: "App",
+  name: 'App',
   components: {
-    HelloWorld,
-  },
-};
+    Post
+  }
+}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  body{
+    font-family: 'monospace', sans-serif;
+  }
+  main{
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+    overflow: hidden;
+  }
+  .navigation{
+    width: 100%;
+    height: 10vh;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background: #000;
+    padding: 30px;
+  }
+  header h1{
+    color: #fff;
+    font-size: 32px;
+  }
+  nav a{
+    margin: 0 20px;
+    text-transform: uppercase;
+    text-decoration: none;
+    color: #fff;
+  }
 </style>
